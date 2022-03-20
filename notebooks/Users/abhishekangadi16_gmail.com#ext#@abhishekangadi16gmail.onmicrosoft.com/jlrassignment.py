2640aa90d781d7816b0beff0de60d91dd372bd29
AANGADI16 <60706241+AANGADI16@users.noreply.github.com>
2022-03-20 14:30:12 +0530

    Update: Test notebooks

--- notebooks/Users/abhishekangadi16_gmail.com#ext#@abhishekangadi16gmail.onmicrosoft.com/jlrassignment.py
+++ notebooks/Users/abhishekangadi16_gmail.com#ext#@abhishekangadi16gmail.onmicrosoft.com/jlrassignment.py
@@ -25,6 +25,7 @@ def transformations(df_joined, df_options):
     """it is the transformation function which is used to apply all the transformations"""
     
     #condition 1: if sales value is 0 or negative then production_cost should be 0
+    #condition 2: if there is exact match then material_cost is production_cost
     df_joined['Production_cost'] = df_joined[['Sales_Price', 'Material_Cost']].apply(lambda x:x['Material_Cost'] if x['Sales_Price']> 0 else 0, axis = 1) 
     
     
@@ -97,7 +98,7 @@ if len(df_options[df_options['Option_Code'].isna()]) > 0 :
 # COMMAND ----------
 
 #joining the two df
-### by joining the condition 2 is satisifed
+
 df_join = dfjoiner(df_base, df_options2,'keybase','keyOptions','left')
 
 #calling the transformation function which applies conditions 1,3,4
@@ -111,39 +112,63 @@ df_transformed['Profit'] = df_transformed['Sales_Price']-df_transformed['Product
 
 # COMMAND ----------
 
+#joining the vehicle data with the transformed data
 df_vehMap = importing('vehicle_line_mapping.csv')
 df_main = dfjoiner(df_transformed, df_vehMap,'Model_x','nameplate_code','left')
-
-# COMMAND ----------
-
-#Cleaning the transformed dataframe and then loading it into ADLS
-
 df_Profit = df_main.drop(['Model_x','keybase','keyOptions','Option_Code','Model_y', 'nameplate_code'], axis = 1)
-ProfitDF=spark.createDataFrame(df_Profit)
-ProfitDF.coalesce(1).write.mode("overwrite").format("csv").option("header","true").csv("/mnt/jlrassig1/outputds")
 
 # COMMAND ----------
 
-#Loading the file from ADLS to check if the write operation executed correctly
-
+#Loading the file from ADLS to check if the data is new or the same.
 path_output = list(dbutils.fs.ls("dbfs:/mnt/jlrassig1/outputds/"))[-1].path
 df_read_check = spark.read.format("csv").option("header","true").option("inferSchema","true").load(path_output).toPandas()
 
-# COMMAND ----------
+
+df_write = pd.merge(df_Profit, df_read_check, how='outer', indicator='Exist')
+df_write = df_write.loc[df_write['Exist'] != 'both']
 
 
 
 # COMMAND ----------
 
-
+#Appending the dataframe in existing one
+if len(df_write) == 0:
+    pass
+else:
+    ProfitDF=spark.createDataFrame(df_write)
+    ProfitDF.coalesce(1).write.mode("append").format("csv").option("header","true").csv("/mnt/jlrassig1/outputds")
 
 # COMMAND ----------
 
+#Grouping the dataframe per options per veicle with aggegrate function as sum.
 
+df_grouped_OC = df_Profit.groupby(['Options_Code','Model_Text'])['Profit'].agg(['sum','count']).reset_index()
+df_grouped_OC_s = df_grouped_OC.sort_values('sum', ascending = False)
+df_grouped_OC_s['Model'] = df_grouped_OC_s['Options_Code']+'-'+df_grouped_OC_s['Model_Text'].str[:4]
+
+
+
+import matplotlib.pyplot as plt
+x = df_grouped_OC_s['Model'][:20]
+y = df_grouped_OC_s['sum'][:20]
+
+plt.bar(x,y)
+plt.xticks(rotation=60)
+plt.xlabel('OPtions_Code')
+plt.ylabel('Total Profit £')
+plt.title('Profit Per Top 20 Options')
+plt.show()
 
 # COMMAND ----------
 
+df_grouped_OC = df_Profit.groupby(['Options_Code','Model_Text'])['Profit'].agg(['sum','count']).reset_index()
+df_grouped_OC_s = df_grouped_OC.sort_values('sum', ascending = False)
+# import matplotlib.pyplot as plt
+df_grouped_OC_s['Options_Code']+'-'+df_grouped_OC_s['Model_Text'].str[:4]
 
+# COMMAND ----------
+
+df_grouped_OC_s['Options_Code']+'-'+df_grouped_OC_s['Model_Text'].str[:4]
 
 # COMMAND ----------
 
@@ -157,36 +182,6 @@ df_read_check = spark.read.format("csv").option("header","true").option("inferSc
 #max profit value
 
 # df_Profit[df_Profit['Profit'] == df_Profit['Profit'].min()]
-
-# COMMAND ----------
-
-df_grouped_OC = df_Profit.groupby(['Options_Code','Model_Text'])['Profit'].agg(['sum','count']).reset_index()
-df_g = df_grouped_OC.sort_values('sum', ascending = False)
-df_g['x-labels'] = df_g['Options_Code'] +"-"+ df_g['Model_Text'].str[:4]
-df_g
-
-# COMMAND ----------
-
-df_grouped_OC = df_Profit.groupby(['Options_Code','Option_Desc','Model_Text','Sales_Price','Production_cost'])['Profit'].agg(['sum','count']).reset_index()
-df_profit_desc = df_grouped_OC.sort_values('sum', ascending = False)
-df_profit_desc
-
-# COMMAND ----------
-
-df_grouped_OC = df_Profit.groupby(['Options_Code','Model_Text'])['Profit'].agg(['sum','count']).reset_index()
-df_g = df_grouped_OC.sort_values('sum', ascending = False)
-df_g['x-labels'] = df_g['Options_Code'] +"-"+ df_g['Model_Text'].str[:4]
-df_bar = df_g.tail(20)
-import matplotlib.pyplot as plt
-x = df_bar['x-labels']
-y = df_bar['sum']
-
-plt.bar(x,y)
-plt.xticks(rotation=60)
-plt.xlabel('Options_Code and Model')
-plt.ylabel('Total Profit £')
-plt.title('Profit Per bottom 20 combinations')
-plt.show()
 
 # COMMAND ----------
 
@@ -207,15 +202,11 @@ plt.show()
 
 # COMMAND ----------
 
-df_Profit[(df_Profit['Model_Text'].str[:4] == 'L494') & (df_Profit['Options_Code'] == '129AH')  ]
-
-# COMMAND ----------
-
 # df_Profit = df_Profit[df_Profit['Model_Text'] == 'L494']
 df_grouped_OC = df_Profit.groupby(['Options_Code'])['Profit'].agg(['sum','count']).reset_index()
 df_bar = df_grouped_OC.sort_values('sum', ascending = False)
 import matplotlib.pyplot as plt
-df_bar2 = df_bar.tail(20)
+df_bar2 = df_bar[:20]
 x = df_bar2['Options_Code']
 y = df_bar2['sum']
 
@@ -230,6 +221,8 @@ plt.show()
 
 import numpy as np
 import matplotlib.pyplot as plt
+df_grouped_OC = df_Profit.groupby(['Options_Code','Model_Text','Sales_Price','Production_cost'])['Profit'].agg(['sum','count']).reset_index()
+df_profit_desc = df_grouped_OC.sort_values('sum', ascending = False)
 
 
 x = df_profit_desc['count']
@@ -238,7 +231,16 @@ y = df_profit_desc['sum']
 # area = (30 * np.random.rand(N))**2  # 0 to 15 point radii
 
 plt.scatter(x, y)
+plt.xlabel('Count[Options+Models]')
+plt.ylabel('Profit £')
+plt.title('Total_Profit vs Count[Options+Models]')
 plt.show()
+
+# COMMAND ----------
+
+df_grouped_OC = df_Profit.groupby(['Options_Code','Option_Desc','Model_Text','Sales_Price','Production_cost'])['Profit'].agg(['sum','count']).reset_index()
+df_profit_desc = df_grouped_OC.sort_values('sum', ascending = False)
+df_profit_desc
 
 # COMMAND ----------
 
